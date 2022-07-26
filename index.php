da1fbab8ff6405c4815b2d23129162dba8b7a011
ingdanyfernando <ing.danyfernando@gmail.com>
2022-07-25 18:47:20 -0600

    Mi Primier Commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+    <link rel="icon" href="icono.png">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <!--BARRA DE NAVEGACION-->
+    <nav>
+        <ul>
+            <li> <a href="default.html">HOME</a> </li>
+            <li> <a href="tablas.html">Tablas</a> </li>
+            <li><a href="listao.html">Listas Ordenadas</a></li>
+            <li> <a href="listau.html">Listas No Ordenadas</a></li>
+        </ul>
+    </nav>
+
+    <!--ENCABEZADO-->
+    <header>
+        <h1>BIENVENIDO</h1>
+    </header>
+
+    <!--CONTENIDO DE LA PAGINA-->
+    <div class="contenedor">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula blandit mollis. Maecenas ut enim risus. Cras sit amet nisi vitae dui luctus consequat eget sed neque. Pellentesque quis odio vel purus iaculis semper. Nunc quis sodales est, a finibus arcu. Curabitur ut lorem vitae arcu mattis fermentum. Cras nibh ligula, scelerisque quis congue et, fermentum vitae purus. Pellentesque congue sollicitudin pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vel lectus in nunc vestibulum tempus sit amet id orci. Aenean convallis libero et viverra euismod. In diam leo, cursus non euismod eget, iaculis ac diam.
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula blandit mollis. Maecenas ut enim risus. Cras sit amet nisi vitae dui luctus consequat eget sed neque. Pellentesque quis odio vel purus iaculis semper. Nunc quis sodales est, a finibus arcu. Curabitur ut lorem vitae arcu mattis fermentum. Cras nibh ligula, scelerisque quis congue et, fermentum vitae purus. Pellentesque congue sollicitudin pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vel lectus in nunc vestibulum tempus sit amet id orci. Aenean convallis libero et viverra euismod. In diam leo, cursus non euismod eget, iaculis ac diam.
+        </p>
+    </div>
+
+    <footer>
+       Todos los derechos reservados a UMG&copy2022 
+    </footer>
+</body>
+</html>
